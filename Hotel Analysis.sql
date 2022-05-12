d8090a7b7941b8535bd8466caad9e5b6c74d8cc1
Subham Saha <82121218+ssgearup@users.noreply.github.com>
2022-05-12 23:16:10 +0530

    Add files via upload

---
+++ Hotel Analysis.sql
@@ -0,0 +1,29 @@
+
+-- Merge table
+with hotels as(
+select * from  [dbo].[2018]
+union
+select * from [dbo].['2019$']
+union
+select * from [dbo].['2020$'])
+
+select * from hotels
+left join
+market_segment$
+on hotels.[market_segment] = market_segment$.market_segment
+left join
+[dbo].[meal_cost$]
+on hotels.[meal] = meal_cost$.meal
+
+-- Average revenue each year per hotel type
+with hotels as(
+select * from  [dbo].[2018]
+union
+select * from [dbo].['2019$']
+union
+select * from [dbo].['2020$'])
+
+select [arrival_date_year],hotel,
+round(sum(((stays_in_weekend_nights+stays_in_week_nights)*adr)),2) as revenue
+from hotels
+group by arrival_date_year ,hotel
